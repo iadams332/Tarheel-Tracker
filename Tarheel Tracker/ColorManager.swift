871dc854a74279b39d97fea2319ef92ccf6892a2
Isaac Adams <isaac.adams332@gmail.com>
2020-11-29 21:15:58 -0500

    Added custom colors and references

---
+++ Tarheel Tracker/ColorManager.swift
@@ -0,0 +1,23 @@
+//
+//  ColorManager.swift
+//  Tarheel Tracker
+//
+//  Created by Isaac Adams on 11/29/20.
+//  Copyright © 2020 Isaac Adams. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color{
+    static let cblue = Color("Carolina Blue")
+    static let cgrey = Color("Carolina Grey")
+    static let cblack = Color("Carolina Black")
+    static let cnavy = Color("Carolina Navy")
+    static let dgrey = Color("Dark Grey")
+}
+
+struct ColorManager_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
