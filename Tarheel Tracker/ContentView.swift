c6b79dd8f70a4ee487033ddf393c82d5f3895de7
Isaac Adams <isaac.adams332@gmail.com>
2020-11-29 20:40:51 -0500

    Initial Commit

---
+++ Tarheel Tracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Tarheel Tracker
+//
+//  Created by Isaac Adams on 11/29/20.
+//  Copyright © 2020 Isaac Adams. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
